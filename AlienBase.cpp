b5f5a473110bcbe4671804a72c54dd5ea7782eba
jvn1567 <56368354+jvn1567@users.noreply.github.com>
2021-06-03 18:17:14 -0700

    Create AlienBase.cpp
    AlienBase cpp file

---
+++ AlienBase.cpp
@@ -0,0 +1,13 @@
+//header
+
+#include "AlienBase.h"
+#include <string>
+using namespace std;
+
+AlienBase::AlienBase() {
+    name = "TEMP NAME";
+}
+
+string AlienBase::getName() {
+    return name;
+}
